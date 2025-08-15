9af19347c1502f02effff0a250e79aeeaa8b07af
Rubenslourenco <alvesrubens926@gmail.com>
2025-08-15 19:41:40 -0300

    feat: add função onTaskClick

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,20 @@
-import { ChevronRight, ChevronRightIcon } from "lucide-react";
+import {  ChevronRightIcon } from "lucide-react";
 
-const Tasks = (prop) => {
+const Tasks = ({tasks, onTaskClick}) => {
+    console.log(tasks);
+    
  
     
     return ( 
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {prop.tasks.map((task) => (
+      {tasks.map((task) => (
         <li key={task.id} className="flex gap-2">
-            <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
-            {task.title}
+            <button 
+            onClick={() => onTaskClick(task.id)}
+            className={`bg-slate-400 text-left w-full text-white p-2 rounded-md ${
+              task.isCompleted && "line-through"
+            }`}>
+                {task.title}
             </button>
             <button className="bg-slate-400  text-white p-2 rounded-md">
                 <ChevronRightIcon />
