05ac51bc6169f2d99575bdcc9c1020cd2044a9a8
Rubenslourenco <alvesrubens926@gmail.com>
2025-08-15 18:47:11 -0300

    feat: add Buttons no componente tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,10 +1,20 @@
+import { ChevronRight, ChevronRightIcon } from "lucide-react";
+
 const Tasks = (prop) => {
  
     
     return ( 
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {prop.tasks.map((task) => (
-        <li key={task.id} className="bg-slate-400 text-white p-2 rounded-md">{task.title}</li>
+        <li key={task.id} className="flex gap-2">
+            <button className="bg-slate-400 text-left w-full text-white p-2 rounded-md">
+            {task.title}
+            </button>
+            <button className="bg-slate-400  text-white p-2 rounded-md">
+                <ChevronRightIcon />
+            </button>
+
+        </li>
       ))}
 
     </ul> 
